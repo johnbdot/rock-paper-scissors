44f2796505204dee86a984b56bb7d0d691258a5e
John Bouakhasith <johnbeedot@gmail.com>
2021-06-05 18:03:40 -0400

    first upload

---
+++ main.js
@@ -0,0 +1,26 @@
+// player chooses
+// computer random choice
+// score = 0
+// if win, score +1
+// if lose, score -1
+// tie if else
+// first to score 10 points, wins. game ends. ask to restart.
+
+let score = 0;
+
+function playerChoice() {
+  // choose rock, paper, scissors
+}
+
+function computerChoice() {
+  // computer randomly chooses rock, paper, scissors from array
+  const random = ["rock", "paper", "scissors"];
+  for (i = 0; i < random.length; i++) {
+    console.log(random[i]);
+  }
+}
+
+function roundPlay(player, computer) {
+  // platerChoice vs computerChoice = results
+  // update score
+}
