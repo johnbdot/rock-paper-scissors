4af0faeea324679d9f7b1ad8c21c789c04300401
John Bouakhasith <johnbeedot@gmail.com>
2021-06-05 18:52:10 -0400

    modified everything

--- main.js
+++ main.js
@@ -1,27 +1,27 @@
+// prompt player name
+// prompt computer name
 // player chooses
 // computer random choice
 // score = 0
 // if win, score +1
 // if lose, score -1
 // tie if else
+// display results
 // first to score 10 points, wins. game ends. ask to restart.
 
 let playerScore = 0;
 let computerScore = 0;
 
-function playerChoice() {
-  // choose rock, paper, scissors
-}
+const playerNameText = document.querySelector(".player-name");
+const computerNameText = document.querySelector(".computer-name");
+const playerScoreText = document.querySelector(".player-score");
+const computerScoreText = document.querySelector("computer-score");
+const resultsText = document.querySelector(".results");
+const compChoiceText = document.querySelector(".computer-choice");
 
-function computerChoice() {
-  // computer randomly chooses rock, paper, scissors from array
-  const random = ["rock", "paper", "scissors"];
-  for (i = 0; i < random.length; i++) {
-    console.log(random[i]);
-  }
-}
-
-function roundPlay(player, computer) {
-  // platerChoice vs computerChoice = results
-  // update score
+function roundStart() {
+  const playerNameInput = prompt("Enter your name:");
+  const computerNameInput = prompt("Enter opponent's name:");
+  playerNameText.textContent = playerNameInput;
+  computerNameText.textContent = computerNameInput;
 }
