329dfae3ce8424b024bd352a8e04b03291ffe523
John Bouakhasith <johnbeedot@gmail.com>
2021-06-05 20:05:36 -0400

    modified everything

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 // click play.
 // enter player name, opponent's name.
+// continue game while scores are below 7.
 // player chooses. computer randmonly chooses.
 // display results. update scores.
-// best out of 7 wins.
+// best out of 7 wins. game ends.
 
 let playerScore = 0;
 let computerScore = 0;
+const maxScore = 7;
 
 const playerNameText = document.querySelector(".player-name");
 const computerNameText = document.querySelector(".computer-name");
@@ -20,11 +22,12 @@ function roundStart() {
 
   playerNameText.textContent = playerNameInput;
   computerNameText.textContent = computerNameInput;
+  compChoiceText.textContent = computerChoice();
 }
 
 function computerChoice() {
   const choice = ["rock", "paper", "scissors"];
   const random = Math.floor(Math.random() * choice.length);
 
-  console.log(choice[random]);
+  return choice[random];
 }
