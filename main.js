096a9a96983ce083813e0fa2591ba6008cbf6393
John Bouakhasith <johnbeedot@gmail.com>
2021-06-06 12:40:13 -0400

    added computer's choice textContent

--- main.js
+++ main.js
@@ -14,15 +14,26 @@ const computerNameText = document.querySelector(".computer-name");
 const playerScoreText = document.querySelector(".player-score");
 const computerScoreText = document.querySelector("computer-score");
 const resultsText = document.querySelector(".results");
-const compChoiceText = document.querySelector(".computer-choice");
+const computerChoiceText = document.querySelector(".computer-choice");
 
 function roundStart() {
   const playerNameInput = prompt("Enter your name:");
   const computerNameInput = prompt("Enter opponent's name:");
 
+  playerChoice();
+
   playerNameText.textContent = playerNameInput;
   computerNameText.textContent = computerNameInput;
-  compChoiceText.textContent = computerChoice();
+}
+
+function playerChoice() {
+  const rock = document.querySelector(".rock");
+  const paper = document.querySelector(".paper");
+  const scissors = document.querySelector(".scissors");
+
+  rock.addEventListener("click", computerText);
+  paper.addEventListener("click", computerText);
+  scissors.addEventListener("click", computerText);
 }
 
 function computerChoice() {
@@ -30,4 +41,8 @@ function computerChoice() {
   const random = Math.floor(Math.random() * choice.length);
 
   return choice[random];
+}
+
+function computerText() {
+  computerChoiceText.textContent = computerChoice();
 }
