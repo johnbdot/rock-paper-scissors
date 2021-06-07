4f1b49f1e311006388cb99db7f93270e6d074e6b
John Bouakhasith <johnbeedot@gmail.com>
2021-06-06 23:38:01 -0400

    updated game logic

--- main.js
+++ main.js
@@ -5,10 +5,11 @@ const maxScore = 7;
 const playerNameText = document.querySelector(".player-name");
 const computerNameText = document.querySelector(".computer-name");
 const playerScoreText = document.querySelector(".player-score");
-const computerScoreText = document.querySelector("computer-score");
-const resultsText = document.querySelector(".results");
+const computerScoreText = document.querySelector(".computer-score");
+const resultsText = document.querySelector(".versus-results");
 const computerChoiceText = document.querySelector(".computer-choice");
 
+// Round start
 function roundStart() {
   const playerNameInput = prompt("Enter your name:", randomName());
   const computerNameInput = prompt("Enter opponent's name:", randomName());
@@ -19,6 +20,7 @@ function roundStart() {
   computerNameText.textContent = computerNameInput;
 }
 
+// Play
 function play() {
   const rockButton = document.querySelector(".rock");
   const paperButton = document.querySelector(".paper");
@@ -28,62 +30,88 @@ function play() {
   paperButton.addEventListener("click", paper);
   scissorsButton.addEventListener("click", scissors);
 
+  // Player chooses Rock
   function rock() {
-    computerText();
-    if (computerChoice() === "Paper") {
-      computerScore++;
-      console.log("lose...");
-    } else if (computerChoice() === "Scissors") {
-      playerScore++;
-      console.log("win!");
-    } else {
-      console.log("tie");
-    }
-    console.log(playerScore + computerScore);
-  }
-
-  function paper() {
-    computerText();
     if (computerChoice() === "Scissors") {
-      computerScore++;
-      console.log("lose...");
-    } else if (computerChoice() === "Rock") {
-      playerScore++;
-      console.log("win!");
-    } else {
-      console.log("tie");
-    }
-    console.log(playerScore + computerScore);
-  }
-
-  function scissors() {
-    computerText();
-    if (computerChoice() === "Rock") {
-      computerScore++;
-      console.log("lose...");
+      computerChoiceText.textContent = "Scissors";
+      win();
     } else if (computerChoice() === "Paper") {
-      playerScore++;
-      console.log("win!");
+      computerChoiceText.textContent = "Paper";
+      lose();
     } else {
-      console.log("tie");
+      computerChoiceText.textContent = "Rock";
+      tie();
     }
-    console.log(playerScore + computerScore);
+
+    playerScoreText.textContent = playerScore;
+    computerScoreText.textContent = computerScore;
   }
 
-  function computerText() {
+  // Player choose Paper
+  function paper() {
     computerChoiceText.textContent = computerChoice();
+    if (computerChoice() === "Rock") {
+      computerChoiceText.textContent = "Rock";
+      win();
+    } else if (computerChoice() === "Scissors") {
+      computerChoiceText.textContent = "Scissors";
+      lose();
+    } else {
+      computerChoiceText.textContent = "Paper";
+      tie();
+    }
+
+    playerScoreText.textContent = playerScore;
+    computerScoreText.textContent = computerScore;
   }
 
-  function computerChoice() {
-    const choice = ["Rock", "Paper", "Scissors"];
-    const random = Math.floor(Math.random() * choice.length);
+  // Player chooses Scissors
+  function scissors() {
+    computerChoiceText.textContent = computerChoice();
+    if (computerChoice() === "Paper") {
+      computerChoiceText.textContent = "Paper";
+      win();
+    } else if (computerChoice() === "Rock") {
+      computerChoiceText.textContent = "Rock";
+      lose();
+    } else {
+      computerChoiceText.textContent = "Scissors";
+      tie();
+    }
 
-    return choice[random];
+    playerScoreText.textContent = playerScore;
+    computerScoreText.textContent = computerScore;
+  }
+
+  // Win
+  function win() {
+    resultsText.textContent = "You WIN! 🥳";
+    playerScore++;
+  }
+
+  // Lose
+  function lose() {
+    resultsText.textContent = "You lose... 😭";
+    computerScore++;
+  }
+
+  // Tie
+  function tie() {
+    resultsText.textContent = "It's a tie! 😅";
   }
 }
 
+// Random name generator for beginning of round
 function randomName() {
   const choice = ["Joe", "Bob", "Sally"];
+  const random = Math.floor(Math.random() * choice.length);
+
+  return choice[random];
+}
+
+// Function for Computer's choice - Rock, Paper or Scissors
+function computerChoice() {
+  const choice = ["Rock", "Paper", "Scissors"];
   const random = Math.floor(Math.random() * choice.length);
 
   return choice[random];
